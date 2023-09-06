8b770c4c9d0b70534d23e8402819624430abfacf
KatherinChun <katherinchun01@gmail.com>
2023-09-06 00:28:35 -0600

    carpetas

--- Pantalla Inicial/pantalla_inicio/src/app/app.module.ts
+++ Pantalla Inicial/pantalla_inicio/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './encabezado/header/header.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { RegistroComponent } from './paginas/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule
