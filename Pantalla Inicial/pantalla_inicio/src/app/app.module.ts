f03449d6358334a0b8afe2df182d7be29cd037aa
KatherinChun <katherinchun01@gmail.com>
2023-09-06 00:52:03 -0600

    login html

--- Pantalla Inicial/pantalla_inicio/src/app/app.module.ts
+++ Pantalla Inicial/pantalla_inicio/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './encabezado/header/header.component';
 import { LoginComponent } from './paginas/login/login.component';
@@ -14,7 +14,8 @@ import { RegistroComponent } from './paginas/registro/registro.component';
     RegistroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Pantalla Inicial/pantalla_inicio/src/app/paginas/login/login.component.ts
+++ Pantalla Inicial/pantalla_inicio/src/app/paginas/login/login.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  nombre: string = '';
+  contrasena: string = '';
+  recordarUsuario: boolean = false;
 
+  iniciarSesion() {
+    // Aquí puedes agregar la lógica para iniciar sesión.
+    // Por ejemplo, puedes enviar los datos al servidor para autenticar al usuario.
+    // También puedes mostrar un mensaje de error si la autenticación falla.
+  }
 }
