ffa1c84616a16e8950d9352dea44075279fea983
KatherinChun <katherinchun01@gmail.com>
2023-09-09 00:59:43 -0600

    registro - enlazado

--- Pantalla Inicial/pantalla_inicio/src/app/app-routing.module.ts
+++ Pantalla Inicial/pantalla_inicio/src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './paginas/login/login.component';
-import { RegistroComponent } from './paginas/registro/registro.component';
+import { RegistroComponent } from './paginas/registro/registro.component';  // importar el componente 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'registro', component: RegistroComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'registro', component: RegistroComponent },     //ruta del componente o pagina
+  { path: '', redirectTo: '/login', pathMatch: 'full' },   // ruta por defecto
 ];
 
 @NgModule({
--- Pantalla Inicial/pantalla_inicio/src/app/app.module.ts
+++ Pantalla Inicial/pantalla_inicio/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './paginas/login/login.component';
-import { RegistroComponent } from './paginas/registro/registro.component';
-import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './paginas/login/login.component'; 
+import { RegistroComponent } from './paginas/registro/registro.component'; // importar el componente
+import { AppRoutingModule } from './app-routing.module'; // importar el modulo de rutas
 
 @NgModule({
   declarations: [
