ec7aee88b1b34fe4cf34f04447ff34a498afdeca
KatherinChun <katherinchun01@gmail.com>
2023-09-14 22:55:09 -0600

    conexion-base de datos

---
+++ Pantalla Inicial/pantalla_inicio/index.js
@@ -0,0 +1,11 @@
+const express = require('express');   //importa express
+var cors = require('cors');       //importa cors
+const connection = require('./connection');  //trae la conexion con la base de datos
+const app = express();
+
+app.use(cors());  //para que no haya problemas de CORS
+app.use(express.urlencoded({ extended: true })); //para que pueda leer los datos que vienen de un formulario
+app.use(express.json());
+
+module.exports = app;   //nos traera todos los datos. 
+
